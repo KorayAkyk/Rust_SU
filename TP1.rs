7a8e7284cd6039d5d597bfddc29efce253dac98a
koray_ak <koray.akyurek6@gmail.com>
2022-05-02 16:02:02 +0200

    ajout correction tp 1

--- TP1.rs
+++ TP1.rs
@@ -8,18 +8,23 @@ fn max(a: i32, b: i32) -> i32 {
 }
 fn bigger(a: i32, b: i32) -> i32 {
     println!("the value is {}", if a > b { a } else { b });
-    0
 }
-/*
-fn mad(a:i32, b:i32, c:i32) -> i32{
-    println!("the result : ", a * b + c)
-}
-//println!("{}",mad(12,92,52))
-*/
-fn main() {
 
+fn bigger_while(a: i32, b: i32) -> i32 {
+    println!("the value is {}", if a > b { a } else { b });
+}
+
+fn main() {
+    //1 fonction max entre 2 chiffres
+    println!("{}",max(2,5));
+
+    //2 focntion max entre 2 chiffres
     println!("{}", bigger(21, 32));
+
+    //fonction sum entre une liste de chiffres
     let a = [1, 2, 3, 4, 5];
     let sum: u8 = a.iter().sum();
     println!("the total sum is: {}", sum);
+
+    println!("boucle while bigger one{}", bigger_while(15, 35));
 }
