e003b615d53c0c1c3008d4906c1050c2e661f329
koray_ak <koray.akyurek6@gmail.com>
2022-05-02 14:30:12 +0200

    fonction sum

--- TP1.rs
+++ TP1.rs
@@ -1,6 +1,3 @@
-
-#![allow(unused)]
-fn main() {
 fn max(a: i32, b: i32) -> i32 {
     if a > b {
         println!("{}",a);
@@ -9,10 +6,20 @@ fn max(a: i32, b: i32) -> i32 {
     println!("{}",b);
     return b;
 }
-let c = "foobar";
-let d = "foo\
-         bar";
-let a = "45";
-assert_eq!(c , d);
+fn bigger(a: i32, b: i32) -> i32 {
+    println!("the value is {}", if a > b { a } else { b });
+    0
+}
+/*
+fn mad(a:i32, b:i32, c:i32) -> i32{
+    println!("the result : ", a * b + c)
+}
+//println!("{}",mad(12,92,52))
+*/
+fn main() {
 
+    println!("{}", bigger(21, 32));
+    let a = [1, 2, 3, 4, 5];
+    let sum: u8 = a.iter().sum();
+    println!("the total sum is: {}", sum);
 }
