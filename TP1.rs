79081f85056994a69a7f9f5fe4a1ad0d049058eb
koray_ak <koray.akyurek6@gmail.com>
2022-05-02 12:36:02 +0200

    first commit test vscode

---
+++ TP1.rs
@@ -0,0 +1,18 @@
+
+#![allow(unused)]
+fn main() {
+fn max(a: i32, b: i32) -> i32 {
+    if a > b {
+        println!("{}",a);
+        return a;
+    }
+    println!("{}",b);
+    return b;
+}
+let c = "foobar";
+let d = "foo\
+         bar";
+let a = "45";
+assert_eq!(c , d);
+
+}
