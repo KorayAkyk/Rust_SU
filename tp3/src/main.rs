459275cc55c29020edf51c729dfc6645a1e04225
koray_ak <koray.akyurek6@gmail.com>
2022-05-03 11:55:23 +0200

    fin tp3 option

--- tp3/src/main.rs
+++ tp3/src/main.rs
@@ -1,10 +1,22 @@
-
+//rust string
 fn isbig(_s: &String, _n: usize) -> bool {
     let lasoeurdeclement = true;
     lasoeurdeclement
 } 
+fn inverse(v: f32) -> f32{
+    1.0 / v
+}
+
+fn safe_inverse(v: f32) -> Option<f32>{
+    if v == 0.0 {
+        None
+    } else {
+        Some(1.0 / v)
+    }
+}
 
 fn main() {
+    //string
     let ricard = String::from("ricard!");
     let vodka = String::from("vodka!");
     let biere = String::from("biere!");
@@ -21,5 +33,22 @@ fn main() {
     println!("{}", isbig(&name, 5));
     
     println!("{}",isbig(&"guillaume".to_string(), 10) == false);
-    
+
+    //option
+    let v = Some(10);
+
+    match v {
+        Some(n) => println!("Pas vide : {}", n),
+        None => println!("Vide"),
+    }
+
+    println!("Inverse de 2 : {}", inverse(2.0));
+    println!("Inverse de 1 : {}", inverse(1.0));
+    println!("Inverse de -1 : {}", inverse(-1.0));
+    println!("Inverse de 0 : {}", inverse(0.0));
+
+    println!("Inverse de 2 : {:?}", safe_inverse(2.0));
+    println!("Inverse de 1 : {:?}", safe_inverse(1.0));
+    println!("Inverse de -1 : {:?}", safe_inverse(-1.0));
+    println!("Inverse de 0 : {:?}", safe_inverse(0.0));
 }
